c35a894707d185b772fcc53fe06c99b7a5af7413
rlawndks4 <rlawndks0423@naver.com>
2023-06-27 13:00:03 +0900

    feat: 배경 애니메이션 개선

--- src/pages/what-we-do.jsx
+++ src/pages/what-we-do.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Paragraph from "@/components/global/Paragraph";
-
+import { motion } from "framer-motion";
+import { fadeIn } from "@/libs/variants";
 const WhatWeDo = () => {
   return (
     <>
